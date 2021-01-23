bb648b9a2cf431862fdef44dd87660966e215f0c
Edison Garcia <egachi@live.com>
2021-01-22 18:20:04 -0600

    Initial Commit

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
